137976e807d1908b77cb5610dfbb3427f82ed54b
gargyogesh4218 <125199173+gargyogesh4218@users.noreply.github.com>
2023-05-04 12:59:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeStringCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a name which to be check if it is palindrome or not");
+            string str= Console.ReadLine();
+
+            char[] arr= str.ToCharArray();
+            Array.Reverse(arr);
+            string str2= new string(arr);
+
+            if (str.ToLower() == str2.ToLower())
+            {
+                Console.WriteLine("palindrome");
+            }
+            else
+                Console.WriteLine("not palindrome");
+
+            Console.ReadKey();
+
+
+                                            
+       
+            
+            
+            
+        }
+
+    }
+}
